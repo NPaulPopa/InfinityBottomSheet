96d76dbafd488a1acacf8c0e5dea0017613cf182
Paul Popa <paul.popa@outlook.com>
2024-01-14 23:43:23 +0000

    Create protocol for customising the behaviour and position of the sheet

---
+++ Sources/InfinityBottomSheet/BottomSheetManager/BottomSheetManagerProtocol.swift
@@ -0,0 +1,27 @@
+//
+//  BottomSheetManagerProtocol.swift
+//  FeedPageVersionTwo
+//
+//  Created by Paul on 25/01/2023.
+//
+
+import UIKit
+
+public protocol BottomSheetManagerProtocol: AnyObject {
+    
+    var dragToDismiss: Bool { get set}
+    var cornerRadius: CGFloat { get set}
+    var smallDetents: CGFloat { get set}
+    var mediumDetents: CGFloat { get set }
+    var largeDetents: CGFloat { get set }
+    var initialHeight: CGFloat { get set }
+    
+    func addSheetChild(child: DraggableItem)
+    
+    var bottomSheetDataSource: InfinityBottomSheetCoordinatorDataSource { get set}
+    
+    func showBottomSheet()
+    func dismissBottomSheet()
+    func setToPosition(_ position: CGFloat,animated: Bool)
+}
+
