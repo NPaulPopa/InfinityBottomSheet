ae3191bb176f8545700fa409d698769996db5cba
Paul Popa <paul.popa@outlook.com>
2024-01-15 23:57:00 +0000

    declare delegate methods

---
+++ Sources/InfinityBottomSheet/BottomSheetProtocols/InfinityBottomSheetDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  InfinityBottomSheetDelegate.swift
+//  FeedPage
+//
+//  Created by Paul Pop on 06/01/2023.
+//
+
+
+import UIKit
+
+///Sheet delegate
+public protocol InfinityBottomSheetCoordinatorDelegate: AnyObject {
+    func bottomSheet(_ container: UIView?, finishTranslationWith animation: @escaping ((_ percent: CGFloat) -> Void) -> Void)
+    func bottomSheet(_ container: UIView?, didChange state: SheetTranslationState)
+    func bottomSheet(_ container: UIView?, didPresent state: SheetTranslationState)
+    
+    func bottomSheet(hasBeganWithVelocity velocity: CGPoint)
+    func bottomSheet(didChangePanWithVelocity velocity: CGPoint)
+
+}
