75e177b03dfa70fa7939b1b68f6e1290b5e342c2
Paul Popa <paul.popa@outlook.com>
2024-01-16 23:57:19 +0000

    Create delegate protocol mapping each state of a bottom sheet

---
+++ Sources/InfinityBottomSheet/BottomSheetManager/BottomSheetManagerDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  BottomSheetManagerDelegate.swift
+//  FeedPageVersionTwo
+//
+//  Created by Paul Pop on 04/02/2023.
+//
+
+import UIKit
+
+protocol BottomSheetManagerDelegate: AnyObject {
+    
+    func bottomSheet(_ container: UIView?, didPresent state: SheetTranslationState)
+    func bottomSheet(_ container: UIView?, didChange state: SheetTranslationState)
+    func bottomSheet(_ container: UIView?, didDismiss state: SheetTranslationState)
+}
+
